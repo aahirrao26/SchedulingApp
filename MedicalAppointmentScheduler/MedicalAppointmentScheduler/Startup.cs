4c5f868d4ad4960ff846ab4b9216e86b9436368e
sunandpujari <sunand.pujari@gmail.com>
2017-03-22 11:39:41 -0400

    First commit

---
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicalAppointmentScheduler.Startup))]
+namespace MedicalAppointmentScheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
