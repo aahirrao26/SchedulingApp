19e9835b922d6f39ab6115cfa348f09b5ab79b08
Amrita <Amrita@AMYSTHINKINGPAD>
2017-03-22 20:14:00 -0400

    Login View

---
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler.Core.Business/AccountManager.cs
@@ -0,0 +1,23 @@
+﻿//using MedicalAppointmentScheduler.Models.EntityModel;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace MedicalAppointmentScheduler.Models.BusinessClass
+//{
+//    public class AccountManager
+//    {
+//        //public bool ValidatedUser(string userName, string password)
+//        //{
+//        //    using (MedicalSchedulerDBEntities dbContext = new MedicalSchedulerDBEntities())
+//        //    {
+//        //        var user = dbContext.User_Login.Where(o => o.Email.ToLower().Equals(userName) && o.Password.Equals(password));
+//        //        if (user.Any())
+//        //            return true;
+//        //        else
+//        //            return false;
+//        //    }
+//        //}
+//    }
+//}
