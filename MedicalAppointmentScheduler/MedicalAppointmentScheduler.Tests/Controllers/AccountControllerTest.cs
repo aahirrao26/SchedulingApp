de25197992f95334c5b8f9d81170091d941553a9
Sean Fox <sfox33@uncc.edu>
2017-03-25 11:28:06 -0400

    Test 2.  Adding AccountControllerTest to git

---
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MedicalAppointmentScheduler.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
