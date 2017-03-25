8de69a98525c0e357afd7079cf179a22ee17cb93
Sean Fox <sfox33@uncc.edu>
2017-03-25 15:43:51 -0400

    Messing with AdminManagerTest

---
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler.Tests/Business Logic/AdminManagerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MedicalAppointmentScheduler.Core.Business;
+
+namespace MedicalAppointmentScheduler.Tests.Business_Logic
+{
+    [TestClass]
+    public class AdminManagerTest
+    {
+        [TestMethod]
+        public void DeleteUser()
+        {
+            int userID = 1;
+            
+        }
+    }
+}
