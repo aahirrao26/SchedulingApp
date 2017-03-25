c6cdcd3920c0ba15f46a592ee255f7a2969148b9
Sean Fox <sfox33@uncc.edu>
2017-03-25 11:53:23 -0400

    Skeleton for AccoutnControllerTest is made

--- MedicalAppointmentScheduler/MedicalAppointmentScheduler.Tests/Controllers/AccountControllerTest.cs
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler.Tests/Controllers/AccountControllerTest.cs
@@ -1,13 +1,127 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+/**
+ * Unit test made for the purpose of testing AccountController
+ * @author: Sean Fox
+ * @date: 3/25/17
+ */
 namespace MedicalAppointmentScheduler.Tests.Controllers
 {
     [TestClass]
     public class AccountControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SignInManager()
+        {
+        }
+
+        [TestMethod]
+        public void UserManager()
+        {
+        }
+
+        [TestMethod]
+        public void Login(string returnUrl)
+        {
+        }
+
+        [TestMethod]
+        public void Login(LoginViewModel model, string returnUrl)
+        {
+        }
+
+        [TestMethod]
+        public void VerifyCode(string provider, string returnUrl, bool rememberMe)
+        {
+        }
+
+        [TestMethod]
+        public void VerifyCode(VerifyCodeViewModel model)
+        {
+        }
+
+        [TestMethod]
+        public void Register()
+        {
+        }
+
+        [TestMethod]
+        public void Register(RegisterViewModel model)
+        {
+        }
+
+        [TestMethod]
+        public void ConfirmEmail(string userId, string code)
+        {
+        }
+
+        [TestMethod]
+        public void ForgotPassword()
+        {
+        }
+
+        [TestMethod]
+        public void ForgotPassword(ForgotPasswordViewModel model)
+        {
+        }
+
+        [TestMethod]
+        public void ForgotPasswordConfirmation()
+        {
+        }
+
+        [TestMethod]
+        public void ResetPassword(string code)
+        {
+        }
+
+        [TestMethod]
+        public void ResetPasword(ResetPasswordViewModel model)
+        {
+        }
+
+        [TestMethod]
+        public void ResetPaswordConfirmation()
+        {
+        }
+
+        [TestMethod]
+        public void ExternalLogin(string provider, string returnUrl)
+        {
+        }
+
+        [TestMethod]
+        public void SendCode(string returnUrl, bool rememberMe)
+        {
+        }
+
+        [TestMethod]
+        public void SendCode(SendCodeViewModel model)
+        {
+        }
+
+        [TestMethod]
+        public void ExternalLoginCallback(string returnUrl)
+        {
+        }
+
+        [TestMethod]
+        public void ExternalLoginConfirmation(ExternalLoginConfirmationViewModel model, string returnUrl)
+        {
+        }
+
+        [TestMethod]
+        public void LogOff()
+        {
+        }
+
+        [TestMethod]
+        public void ExternalLoginFailure()
+        {
+        }
+
+        [TestMethod]
+        public void Dispose(bool disposing)
         {
         }
     }
