fa90e42f6221d31208e98411a03701f704b16112
Amrita <aahirrao@uncc.edu>
2017-03-24 23:34:23 -0400

    added Validation message and minor cosmetic changes

---
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentScheduler.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
