f4cc682c540fa0df7d86a0eeae405162d88ed3dc
Sean Fox <sfox33@uncc.edu>
2017-04-06 20:24:46 -0400

    Just opening the files.

--- MedicalAppointmentScheduler/MedicalAppointmentScheduler.Core.Business/AdminManager.cs
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler.Core.Business/AdminManager.cs
@@ -1,6 +1,7 @@
 ﻿using MedicalAppointmentScheduler.Core.Data;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,13 +14,70 @@ namespace MedicalAppointmentScheduler.Core.Business
 
         public AdminManager(MedicalSchedulerDBEntities _dbContext)
         {
-            this.dbContext = _dbContext; 
+            this.dbContext = _dbContext;
         }
+
+        /// <summary>
+        /// This method will delete the user 
+        /// </summary>
+        /// <param name="userId"></param>
         public void DeleteUser(int userId)
-        {        
-             UserDetails userDetails = dbContext.UserDetails.Find(userId);
-             dbContext.UserDetails.Remove(userDetails);
-             dbContext.SaveChanges();
+        {
+            try
+            {
+                //Delete login entry
+                UserLogin userLogin = dbContext.UserLogins.SingleOrDefault(u => u.UserID == userId);
+                if (userLogin != null)
+                {
+                    dbContext.UserLogins.Remove(userLogin);
+                }
+
+                //delete user address
+                UserAddress userAddress = dbContext.UserAddresses.SingleOrDefault(u => u.UserID == userId);
+                if (userAddress != null)
+                {
+                    dbContext.UserAddresses.Remove(userAddress);
+                }
+
+                //delete user details
+                UserDetails userDetails = dbContext.UserDetails.Find(userId);
+                dbContext.UserDetails.Remove(userDetails);
+
+                dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// This method update the user details 
+        /// </summary>
+        /// <param name="userDetails"></param>
+        public void EditUser(UserDetails userDetails)
+        {
+            dbContext.Entry(userDetails).State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+
+
+        /// <summary>
+        /// This is use to Create new user
+        /// </summary>
+        /// <param name="userDetails"></param>
+        public void CreateUser(UserDetails userDetails)
+        {
+            dbContext.UserDetails.Add(userDetails);
+            dbContext.SaveChanges();
+        }
+
+        /**
+         * Used to retrieve data.  For testing purposes only.
+         */
+        public int getUserDetails(int id)
+        {
+            var roleId = dbContext.UserDetails.Where(o => o.ID.Equals(id)).Select(u => u.ID).SingleOrDefault();
+            return roleId;
         }
     }
 }
---
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler.Tests/BusinessLayer/AdminManagerTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+using MedicalAppointmentScheduler.Core.Data;
+using MedicalAppointmentScheduler.Core.Business;
+using MedicalAppointmentScheduler.Models.BusinessClass;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+
+namespace MedicalAppointmentScheduler.Tests.Controllers
+{
+    [TestClass]
+    public class AdminManagerTest
+    {
+        private AdminManager adminManager;
+
+        [TestInitialize]
+        public void setUpData()
+        {
+            var UserLoginData = new List<UserLogin>
+            {
+                new UserLogin { ID = 1, Email="aa@gmail.com", Password="12345", UserID = 1},
+                new UserLogin { ID = 2, Email="bb@gmail.com", Password="12345", UserID = 2}
+            }.AsQueryable();
+
+            var userRoleData = new List<UserRole>
+            {
+                new UserRole {ID=1, RoleName="Administrator"}
+            }.AsQueryable();
+
+            var userDetailsData = new List<UserDetails>
+            {
+                new UserDetails {ID=1,RoleID=1}
+            }.AsQueryable();
+
+            var mockUserLoginSet = new Mock<DbSet<UserLogin>>();
+            mockUserLoginSet.As<IQueryable<UserLogin>>().Setup(m => m.Provider).Returns(UserLoginData.Provider);
+            mockUserLoginSet.As<IQueryable<UserLogin>>().Setup(m => m.Expression).Returns(UserLoginData.Expression);
+            mockUserLoginSet.As<IQueryable<UserLogin>>().Setup(m => m.ElementType).Returns(UserLoginData.ElementType);
+
+            var mockUserRoleSet = new Mock<DbSet<UserRole>>();
+            mockUserRoleSet.As<IQueryable<UserRole>>().Setup(m => m.Provider).Returns(userRoleData.Provider);
+            mockUserRoleSet.As<IQueryable<UserRole>>().Setup(m => m.Expression).Returns(userRoleData.Expression);
+            mockUserRoleSet.As<IQueryable<UserRole>>().Setup(m => m.ElementType).Returns(userRoleData.ElementType);
+
+            var mockUserDetailsSet = new Mock<DbSet<UserDetails>>();
+            mockUserDetailsSet.As<IQueryable<UserDetails>>().Setup(m => m.Provider).Returns(userDetailsData.Provider);
+            mockUserDetailsSet.As<IQueryable<UserDetails>>().Setup(m => m.Expression).Returns(userDetailsData.Expression);
+            mockUserDetailsSet.As<IQueryable<UserDetails>>().Setup(m => m.ElementType).Returns(userDetailsData.ElementType);
+
+            var mockContext = new Mock<MedicalSchedulerDBEntities>();
+            mockContext.Setup(m => m.UserLogins).Returns(mockUserLoginSet.Object);
+            mockContext.Setup(m => m.UserDetails).Returns(mockUserDetailsSet.Object);
+            mockContext.Setup(m => m.UserRoles).Returns(mockUserRoleSet.Object);
+
+            adminManager = new AdminManager(mockContext.Object);
+        }
+
+        [TestMethod]
+        public void CreateUserTest() {
+            UserDetails user = new UserDetails();
+            user.ID = 2;
+            user.FirstName = "Sean";
+            user.LastName = "Fox";
+            user.EmailAdress = "33@gmail.com"; 
+            user.RoleID = 3;
+            adminManager.CreateUser(user);
+
+            var test = adminManager.getUserDetails(user.ID);
+            Assert.AreEqual(2, test);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+
+        }
+    }
+}
