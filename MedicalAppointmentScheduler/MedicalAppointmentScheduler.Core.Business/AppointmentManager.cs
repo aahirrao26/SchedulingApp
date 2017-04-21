05e86a904b582e1177475e80c1aff98b841c7456
Sean Fox <sfox33@uncc.edu>
2017-04-20 20:48:34 -0400

    Trying to update classes

--- MedicalAppointmentScheduler/MedicalAppointmentScheduler.Core.Business/AppointmentManager.cs
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler.Core.Business/AppointmentManager.cs
@@ -11,9 +11,12 @@ namespace MedicalAppointmentScheduler.Core.Business
     {
         bool BookAppointment(Appointment newAppointment);
         List<UserDetails> GetDoctorList();
-
+        List<AvailableSlots> GetAvailableSlots(int doctorID, DateTime date);
+        List<Appointment> GetAppointmentList();
+        Appointment FindAppointment(int? AppointmentID);
+        void DeleteAppointment(int AppointmentID);
     }
-    public class AppointmentManager:IAppointmentManager
+    public class AppointmentManager : IAppointmentManager
     {
 
         private MedicalSchedulerDBEntities dbContext;
@@ -36,9 +39,6 @@ namespace MedicalAppointmentScheduler.Core.Business
         {
             try
             {
-                //TODO: Remove hardcorded value for slot
-                newAppointment.SlotID = 1;
-                               
                 dbContext.Appointments.Add(newAppointment);
                 dbContext.SaveChanges();
                 return true;
@@ -54,8 +54,76 @@ namespace MedicalAppointmentScheduler.Core.Business
         /// </summary>
         /// <returns></returns>
         public List<UserDetails> GetDoctorList()
-        { 
+        {
             return dbContext.UserDetails.Where(role => role.RoleID == (int)ApplicationRole.Doctor).ToList();
         }
+
+        /// <summary>
+        /// This gets the list of all the slots based on the provided date and doctor's ID
+        /// </summary>
+        /// <param name="doctorID"></param>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public List<AvailableSlots> GetAvailableSlots(int doctorID, DateTime date)
+        {
+            List<AvailableSlots> availableSlots = (from slots in dbContext.Slots
+                                                   join appointments in dbContext.Appointments
+                                                   on slots.ID equals appointments.SlotID
+                                                   into sa
+                                                   from t in sa.Where(f => f.DoctorID == doctorID && f.Date == date.Date).DefaultIfEmpty()
+                                                   where t == null
+                                                   select new AvailableSlots { ID = slots.ID, EndTime = slots.EndTime, StartTime = slots.StartTime }).ToList();
+            if (date == DateTime.Today)
+            {
+                var currentTime = DateTime.Now.TimeOfDay;
+                availableSlots = availableSlots.Where(f => f.StartTime > currentTime).ToList();
+            }
+            return availableSlots;
+        }
+
+        /// <summary>
+        /// Get the list of all appointments order by appointment date and starttime
+        /// </summary>
+        /// <returns></returns>
+        public List<Appointment> GetAppointmentList()
+        {
+            List<Appointment> appointmentList = dbContext.Appointments.Where(u => u.Date >= DateTime.Today).OrderByDescending(u => u.Date).ThenBy(u => u.L_Slots.StartTime).ToList();
+            return appointmentList;
+        }
+
+        /// <summary>
+        /// Find appointment for the passed appointmentID
+        /// </summary>
+        /// <param name="AppointmentID"></param>
+        /// <returns></returns>
+        public Appointment FindAppointment(int? AppointmentID)
+        {
+            Appointment appointment = dbContext.Appointments.Find(AppointmentID);
+            return appointment;
+        }
+
+        /// <summary>
+        /// Delete appointment
+        /// </summary>
+        /// <param name="AppointmentID"></param>
+        public void DeleteAppointment(int AppointmentID)
+        {
+            try
+            {
+                //Delete appointment entry
+                Appointment appointment = dbContext.Appointments.SingleOrDefault(u => u.ID == AppointmentID);
+                if (appointment != null)
+                {
+                    dbContext.Appointments.Remove(appointment);
+                }
+
+                dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+
     }
 }
