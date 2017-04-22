eb7513275db349a0dea0421669e801eb5d9ac32f
sunandpujari <sunand.pujari@gmail.com>
2017-04-22 15:19:18 -0400

    Task #132,133: upcoming appointments for doctor

--- MedicalAppointmentScheduler/MedicalAppointmentScheduler/Controllers/DoctorController.cs
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler/Controllers/DoctorController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using MedicalAppointmentScheduler.Core.Data;
 using MedicalAppointmentScheduler.Core.Business;
+using PagedList;
 
 namespace MedicalAppointmentScheduler.Controllers
 {
@@ -16,16 +17,22 @@ namespace MedicalAppointmentScheduler.Controllers
     public class DoctorController : Controller
     {
         private ISearchManager SearchManager;
+        IAppointmentManager appointmentManager;
+        int pageSize = 5;
+        int pageIndex = 1;
 
         public DoctorController()
         {
             SearchManager = new SearchManager();
+            appointmentManager = new AppointmentManager();
         }
 
-        public DoctorController(ISearchManager _SearchManager)
+        public DoctorController(ISearchManager _SearchManager, IAppointmentManager _appointmentManager)
         {
             SearchManager = _SearchManager;
+            appointmentManager = _appointmentManager;
         }
+
 
         // GET: Doctor
         public ActionResult Index()
@@ -42,6 +49,19 @@ namespace MedicalAppointmentScheduler.Controllers
                 return View(SearchManager.GetPatientList(firstName, lastName));
             else
                 return View(SearchManager.GetPatientList(firstName, lastName));
+        }
+
+        /// <summary>
+        /// Creates a view displaying all of the doctors's appointments and detailes of those appointmentsViewUpcomingAppointment
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ViewUpcomingAppointment(int? page)
+        {
+            pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            int doctorID = Convert.ToInt32(Session["LoggedInUser"]);   //Gets doctor ID of user
+            List<Appointment> appointmentList = appointmentManager.GetUpcomingAppointmentsForDoctor(doctorID); //Retrieve all appointments for user
+
+            return View(appointmentList.ToPagedList(pageIndex, pageSize));
         }
     }
 }
