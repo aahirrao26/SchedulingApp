83f0bf94176fd88f9e62a68e11c1a0b3ac763bce
SHAKTI <SHAKTI@SHAKTI-PC>
2017-04-22 23:09:14 -0400

    Business Logic for medical conditions

---
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler.Core.Data/Models/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentScheduler.Core.Data.Models
+{
+    [MetadataType(typeof(PatientMetadata))]
+    public partial class Patient
+    {
+
+    }
+
+    public class PatientMetadata
+    {
+        public int PatientID { get; set; }
+
+        public String Gender { get; set; }
+
+        public float HeightFeet { get; set; }
+
+        public float HeightInches{ get; set; }
+
+        public float Weight { get; set; }
+
+        public virtual UserDetails User_Details { get; set; }
+
+        public virtual Type Type { get; set; }
+    }
+}
---
+++ MedicalAppointmentScheduler/MedicalAppointmentScheduler.Core.Data/Models/PatientConditions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentScheduler.Core.Data.Models
+{
+    [MetadataType(typeof(PatientConditionsMetadata))]
+    public partial class PatientConditions
+    {
+
+    }
+
+    public class PatientConditionsMetadata
+    {
+        public int PatientID { get; set; }
+        
+        public int ID { get; set; }
+
+        public virtual UserDetails User_Details { get; set; }
+
+        public virtual Condition Condition { get; set; }
+    }
+}
